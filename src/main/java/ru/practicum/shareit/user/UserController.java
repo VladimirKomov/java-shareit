93356a0a603d662c71eb940d3c3e682c28f6d670
Vladimir <v-e-komov@yandex.ru>
2022-12-28 14:11:54 +0300

    feat: добавить фукнционал usercontroller

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,7 +1,11 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.util.Collection;
 
 /**
  * TODO Sprint add-controllers.
@@ -9,4 +13,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @PatchMapping("/{userId}")
+    public User updateUser(@PathVariable long userId, @RequestBody User user) {
+        return userService.updateUser(userId, user);
+    }
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable long userId) {
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping
+    public Collection<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable long userId) {
+        userService.deleteUser(userId);
+    }
+
 }
--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -1,5 +1,40 @@
 package ru.practicum.shareit.user.service;
 
-public class UserServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.storage.UserStorage;
 
+import java.util.Collection;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserStorage userStorage;
+    @Override
+    public User addUser(User user) {
+        return userStorage.addUser(user);
+    }
+
+    @Override
+    public User updateUser(long userId, User user) {
+        return userStorage.updateUser(userId, user);
+    }
+
+    @Override
+    public void deleteUser(long userId) {
+        userStorage.deleteUser(userId);
+    }
+
+    @Override
+    public User getUserById(long userId) {
+        return userStorage.getUserById(userId).orElseThrow(() -> new NotFoundException());
+    }
+
+    @Override
+    public Collection<User> getAllUsers() {
+        return userStorage.getAllUsers();
+    }
 }
