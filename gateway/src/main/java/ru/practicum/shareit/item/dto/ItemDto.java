eb4ca869076406733e6068243c475f2284916ba6
Владимир Комов <v-e-komov@yandex.ru>
2023-02-10 17:59:26 +0300

    feat: add item geteway

---
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.item.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ItemDto {
+    private long id;
+    @NotNull
+    @NotBlank
+    private String name;
+    @NotNull
+    @NotBlank
+    private String description;
+    @NotNull
+    private Boolean available;
+    private UserDto owner;
+    private ItemRequestDto request;
+    private long requestId;
+}
